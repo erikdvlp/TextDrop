442cac9955ce71d3d408eb815d62f2e5c51f8b9c
Erik Taylor <60715210+erikdvlp@users.noreply.github.com>
2021-02-24 14:12:36 -0500

    Add view raw

--- web.py
+++ web.py
@@ -16,8 +16,8 @@ def index():
 	#return index page
 	return render_template("index.html")
 
-#view post page
-@app.route("/p/<postId>")
+#main post page
+@app.route("/<postId>")
 def viewPost(postId):
 	#query database
 	resp = network.getPost(postId)
@@ -30,10 +30,25 @@ def viewPost(postId):
 	#return post page
 	return render_template("post.html", postId = p.id, postTextByLine = postTextByLine, postSize = postSize, postTime = p.time)
 
+#raw post page
+@app.route("/raw/<postId>")
+def viewRawPost(postId):
+	#query database
+	resp = network.getPost(postId)
+	if (resp == None):
+		return redirect(url_for("noPage"))
+	p = post.Post(resp["text"], postId, resp["time"])
+	#manipulate strings for display
+	postTextByLine = p.text.split("\n")
+	#return raw post page
+	return render_template("raw.html", postTextByLine = postTextByLine)
+
+#404
 @app.route("/404")
 def noPage():
 	return render_template("404.html")
 
+#catch-all
 @app.errorhandler(404)
 def handle_404(e):
 	return redirect(url_for("noPage"))
